d72f009f6d3a8b58b46fa03471b29a60ac081b5d
Huddersfield Snooker <HBSA.MS@outlook.com>
2021-05-02 10:54:52 +0100

    4 week rrule

--- HBSA Web Application/My Project/AssemblyInfo.vb
+++ HBSA Web Application/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.10.111.10419")>
-<Assembly: AssemblyFileVersion("3.10.111")>
+<Assembly: AssemblyVersion("3.10.112.10501")>
+<Assembly: AssemblyFileVersion("3.10.112")>
