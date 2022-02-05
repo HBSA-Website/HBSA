b9fbccb70f26b2f9097cdb8fba5ee0c0b4b8ae34
Huddersfield Snooker <HBSA.MS@outlook.com>
2022-02-05 12:32:51 +0000

    20036 - Make new contact live

--- HBSA Web Application/My Project/AssemblyInfo.vb
+++ HBSA Web Application/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.10.159.20035")>
+<Assembly: AssemblyVersion("3.10.159.20036")>
 <Assembly: AssemblyFileVersion("3.10.159")>
