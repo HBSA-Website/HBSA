819c927fff1a2d0b2bcd0dca6f7cd61062981777
Huddersfield Snooker <HBSA.MS@outlook.com>
2022-08-14 16:57:45 +0100

    Fix orphaned players

--- SQL Scripts/script fix orphaned players.sql
+++ SQL Scripts/script fix orphaned players.sql
@@ -1,10 +1,15 @@
 use hbsa
 go
-update players
-	set sectionID=T.SectionID
+--update players
+--	set sectionID=T.SectionID
+
+select dbo.FullPlayername(Forename,initials,surname), [Club Name], P.Team, LeagueID, P.sectionID, T.SectionID 
  from players P
 		join clubs C on C.ID=P.ClubID
 		join teams T on T.ClubID=P.ClubID and T.Team=P.Team and T.SectionID between 1 and 5
-where p.sectionID=6
+
+
+where P.LeagueID=1
+  and P.SectionID <> T.sectionID
 
 
